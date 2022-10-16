70c8ae705e8e7d6abd26978bf4fa7bdd1c1799b0
Raquel Lima <rrlima.raquel@gmail.com>
2022-10-16 17:50:10 +0200

    removed irrelevant files from project; installed mui dependency; created component folder; created Layout;

---
+++ youload/src/components/Layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+
+const Layout = (): JSX.Element => {
+  return (
+    <Grid container>
+      <Grid item xs={12}></Grid>
+      <Grid item xs={12}></Grid>
+    </Grid>
+  )
+}
+
+export { Layout }
